5f6dcf6365f3cde20b788040e0b108105ca22a21
Evelyn Binkard <evelyn.binkard@gmail.com>
2020-02-19 20:11:08 -0600

    added model object type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,16 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, 
+    GraphQLString,
+    GraphQLBoolean } = graphql;
 
-
+const ModelType = new GraphQLObjectType({
+    name: 'Model',
+    fields: () => ({
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        checkedIn: { type: GraphQLBoolean },
+    })
+});
