4f2ffd8f2f0fd7482ca63299550faa647afd9c40
Evelyn Binkard <evelyn.binkard@gmail.com>
2020-03-22 11:22:25 -0500

    create look list component

---
+++ client/src/components/LookList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class LookList extends Component {
+  render(){
+    return (
+      <>
+        <ul id="look-list">
+            <li>Look Description</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default LookList;
