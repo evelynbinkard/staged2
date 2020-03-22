0a7788a00fb1abbe71fa759dfd9729d0beb80742
Evelyn Binkard <evelyn.binkard@gmail.com>
2020-03-22 11:30:38 -0500

    set up apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 //components
 import LookList from './components/LookList';
 
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 class App extends Component {
   render(){
     return (
-      <div id="main">
-        <h1>Models' Looks</h1>
-        <LookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Models' Looks</h1>
+          <LookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
