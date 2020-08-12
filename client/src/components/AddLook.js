ea9b14e03a579e1be4d11970b71cc7afca4659d8
Evelyn Binkard <evelyn.binkard@gmail.com>
2020-08-11 21:07:48 -0500

    begin mutation for add look

--- client/src/components/AddLook.js
+++ client/src/components/AddLook.js
@@ -3,7 +3,18 @@ import { graphql } from 'react-apollo';
 import { getRunwayModelsQuery } from '../queries/queries';
 
 class AddLook extends Component {
-  displayRunwayModels(){
+
+    constructor(props){
+        super(props);
+        this.state = {
+            lookDescription: '',
+            collection: '',
+            orderInShow: '',
+            assignedRunwayModelId: ''
+        }
+    }
+
+    displayRunwayModels(){
       var data = this.props.data;
 
       if (data.loading) {
@@ -15,37 +26,43 @@ class AddLook extends Component {
             )
         })
       }
-  }
-  render(){
-    return (
-      <>
-        <form id="add-look-form">
-           
-            <div className="field">
-                <label>Look Description</label>
-                <input type="text"></input>
-            </div>
-            <div className="field">
-                <label>Collection</label>
-                <input type="text"></input>
-            </div>
-            <div className="field">
-                <label>Order in Collection</label>
-                <input type="text"></input>
-            </div>
-            <div className="field">
-                <label>Assign Model</label>
-                <select>
-                    <option>Select Model</option>
-                    {this.displayRunwayModels()}
-                </select>
-            </div>
+    }
 
-        </form>
-        
-        <button>+</button>
-      </>
-    );
+    submitForm(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render(){
+        return (
+        <>
+            <form id="add-look-form" onSubmit={this.submitForm.bind(this)}>
+            
+                <div className="field">
+                    <label>Look Description</label>
+                    <input type="text" onChange={(e) => this.setState({lookDescription: e.target.value})}></input>
+                </div>
+                <div className="field">
+                    <label>Collection</label>
+                    <input type="text" onChange={(e) => this.setState({collection: e.target.value})}></input>
+                </div>
+                <div className="field">
+                    <label>Order in Collection</label>
+                    <input type="text" onChange={(e) => this.setState({orderInShow: e.target.value})}></input>
+                </div>
+                <div className="field">
+                    <label>Assign Model</label>
+                    <select onChange={(e) => this.setState({assignedRunwayModelId: e.target.value})}>
+                        <option>Select Model</option>
+                        {this.displayRunwayModels()}
+                    </select>
+                </div>
+                <button>+</button>
+
+            </form>
+            
+        </>
+        );
   }
 }
 
